2ac8bcf7ce51b77516cc9ee0314e95011dbe7468
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-05-02 12:59:51 -0600

    Create initial migrations

---
+++ db/migrate/20220502185135_create_invoice_items.rb
@@ -0,0 +1,13 @@
+class CreateInvoiceItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :invoice_items do |t|
+      t.references :item
+      t.references :invoice
+      t.integer :quantity
+      t.integer :unit_price
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
